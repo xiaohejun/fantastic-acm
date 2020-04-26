e28498990fff5cd3120406af8d78eb875f8f83c6
何军 <jun.he@chaitin.com>
2020-04-26 22:26:01 +0800

    add cplus template

---
+++ cplus_template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+#define dbg(x) cout << "line-(" << __LINE__ << "): " << #x"=" << x << endl;
+
+int main(){
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n;
+    while(cin >> n) dbg(n)
+    return 0;
+}

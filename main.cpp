696df5fc15ade2db00ea63d912f224430f2e0ea2
何军 <jun.he@chatin.com>
2020-05-31 22:01:33 +0800

    添加一个公用的main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+#define dbg(x) cout << "line-(" << __LINE__ << "): " << #x"=" << x << endl;
+
+int main(){
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(0); cin.tie(0);
+    int64_t x;
+    cin >> x;
+    dbg(x)
+    return 0;
+}

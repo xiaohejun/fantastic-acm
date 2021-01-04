c02b2d12111427d8a9f321615b0dda7dd77d1d22
何军 <2931094367@qq.com>
2021-01-04 23:25:24 +0800

    每日一题完成

---
+++ leetcode/509.斐波那契数.c
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=c
+ *
+ * [509] 斐波那契数
+ *
+ * https://leetcode-cn.com/problems/fibonacci-number/description/
+ *
+ * algorithms
+ * Easy (66.20%)
+ * Likes:    223
+ * Dislikes: 0
+ * Total Accepted:    133.2K
+ * Total Submissions: 195.3K
+ * Testcase Example:  '2'
+ *
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * 
+ * 
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 
+ * 
+ * 给你 n ，请计算 F(n) 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+int fib(int n){
+    #define MAX_N 31
+    int f[MAX_N];
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+}
+// @lc code=end
+
